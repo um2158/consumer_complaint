6e6b91ca06024fd293b0c87f2100bcf55e8db99d
um2158 <um2158@columbia.edu>
2023-05-12 00:02:15 +0530

    adding Readme file

---
+++ setup.py
@@ -0,0 +1,36 @@
+from setuptools import setup, find_packages
+from typing import List
+
+# Declaring variables for setup functions
+PROJECT_NAME = "finance-complaint"
+VERSION = "0.0.1"
+AUTHOR = "Uday Mukhija"
+DESRCIPTION = "End to end NLP pipeline"
+REQUIREMENT_FILE_NAME = "requirements.txt"
+
+HYPHEN_E_DOT ="-e ."
+
+
+def get_requirements_list(file_path=REQUIREMENT_FILE_NAME) -> List[str]:
+    """
+    Description: This function is going to return list of requirement
+    mention in requirements.txt file
+    return This function is going to return a list which contain name
+    of libraries mentioned in requirements.txt file
+    """
+    with open(file_path) as requirement_file:
+        requirement_list = requirement_file.readlines()
+        requirement_list = [requirement_name.replace("\n", "") for requirement_name in requirement_list]
+        if HYPHEN_E_DOT in requirement_list:
+            requirement_list.remove(HYPHEN_E_DOT)
+        return requirement_list
+
+
+setup(
+    name=PROJECT_NAME,
+    version=VERSION,
+    author=AUTHOR,
+    description=DESRCIPTION,
+    packages=find_packages(),
+    install_requires=get_requirements_list()
+)
